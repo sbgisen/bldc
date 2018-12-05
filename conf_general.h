b1428cd545b7418b195ecf18f5a5d36531502f44
nyxrobotics <takaaki.numai01@g.softbank.co.jp>
2018-12-05 15:14:45 +0900

    add gisen definitions

--- conf_general.h
+++ conf_general.h
@@ -54,7 +54,7 @@
 	!defined(HW_VERSION_48) && !defined(HW_VERSION_49) && !defined(HW_VERSION_410) && \
 	!defined(HW_VERSION_60) && !defined(HW_VERSION_R2) && !defined(HW_VERSION_VICTOR_R1A) && \
 	!defined(HW_VERSION_DAS_RS) && !defined(HW_VERSION_PALTA) && !defined(HW_VERSION_RH) && \
-	!defined(HW_VERSION_TP) && !defined(HW_VERSION_75_300) && !defined(HW_VERSION_MINI4) && \
+	!defined(HW_VERSION_TP) && !defined(HW_VERSION_75_300) && !defined(HW_VERSION_MINI4) && !defined(HW_VERSION_GISEN) && \
 	!defined(HW_VERSION_DAS_MINI)
 //#define HW_VERSION_40
 //#define HW_VERSION_45
