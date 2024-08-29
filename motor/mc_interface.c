6549d382cffb22da93a0c1faa809c446f8051f43
saitenntaisei <saitenntaisei@gmail.com>
2024-08-29 17:18:35 +0900

    wip

--- motor/mc_interface.c
+++ motor/mc_interface.c
@@ -1486,7 +1486,7 @@ void mc_interface_update_pid_pos_offset(float angle_now, bool store) {
 			angle_potentiometer_sum / adc_avg_window_size;
 		mcconf->p_pid_offset = angle_potentiometer-angle_now;
 	}else{
-		mcconf->p_pid_offset += mc_interface_get_pid_pos_now()-angle_now;
+		mcconf->p_pid_offset += mc_interface_get_pid_pos_now() - angle_now;
 	}
 
 	utils_norm_angle(&mcconf->p_pid_offset);
